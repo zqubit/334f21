2ef1824b40f20bf3b7da3b8977c8475759e90444
Guannan Zhao <guannanzhao2023@u.northwestern.edu>
2023-01-20 16:14:41 -0600

    Add starter code files

---
+++ MidtermProject1/block.rs
@@ -0,0 +1,104 @@
+//! This module defines the layout of a block.
+//! 
+//! You do not need to modify this file, except for the `default_difficulty` function.
+//! Please read this file to understand the structure of a block.
+
+use serde::{Serialize, Deserialize};
+use crate::crypto::hash::{H256, Hashable};
+use crate::transaction::SignedTransaction as Transaction;
+
+/// The block header
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Header {
+    pub parent: H256,
+    pub nonce: u32,
+    pub difficulty: H256,
+    pub timestamp: u128,
+    pub merkle_root: H256,
+}
+
+/// Transactions contained in a block
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Content {
+    pub transactions: Vec<Transaction>,
+}
+
+/// A block in the blockchain
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Block {
+    pub header: Header,
+    pub content: Content,
+}
+
+/// Returns the default difficulty, which is a big-endian 32-byte integer.
+/// - Note: a valid block must satisfy that `block.hash() <= difficulty`.
+///   In other words, the _smaller_ the `difficulty`, the harder it actually is to mine a block!
+fn default_difficulty() -> [u8; 32] {
+    // TODO: it's up to you to determine an appropriate difficulty.
+    // For example, after executing the code below, `difficulty` represents the number 256^31.
+    //
+    // let mut difficulty = [0u8; 32];
+    // difficulty[0] = 1;
+    // difficulty
+    unimplemented!()
+}
+
+impl Block {
+    /// Construct the (totally deterministic) genesis block
+    pub fn genesis() -> Block {
+        let transactions: Vec<Transaction> = vec![];
+        let header = Header {
+            parent: Default::default(),
+            nonce: 0,
+            difficulty: default_difficulty().into(),
+            timestamp: 0,
+            merkle_root: Default::default(),
+        };
+        let content = Content { transactions };
+        Block { header, content }
+    }
+}
+
+impl Hashable for Header {
+    /// Hash the block header using SHA256.
+    fn hash(&self) -> H256 {
+        let bytes = bincode::serialize(&self).unwrap();
+        ring::digest::digest(&ring::digest::SHA256, &bytes).into()
+    }
+}
+
+impl Hashable for Block {
+    /// Hash only the block header.
+    fn hash(&self) -> H256 {
+        self.header.hash()
+    }
+}
+
+/* Please add the following code snippet into `src/transaction.rs`: */
+// impl Hashable for Transaction {
+//     fn hash(&self) -> H256 {
+//         let bytes = bincode::serialize(&self).unwrap();
+//         ring::digest::digest(&ring::digest::SHA256, &bytes).into()
+//     }
+// }
+
+#[cfg(any(test, test_utilities))]
+pub mod test {
+    use super::*;
+    use crate::crypto::hash::H256;
+    use crate::crypto::merkle::MerkleTree;
+
+    pub fn generate_random_block(parent: &H256) -> Block {
+        let transactions: Vec<Transaction> = vec![Default::default()];
+        let root = MerkleTree::new(&transactions).root();
+        let header = Header {
+            parent: *parent,
+            nonce: rand::random(),
+            difficulty: default_difficulty().into(),
+            timestamp: rand::random(),
+            merkle_root: root,
+        };
+        let content = Content { transactions };
+        Block { header, content }
+    }
+}
---
+++ MidtermProject1/blockchain.rs
@@ -0,0 +1,99 @@
+//! This module implements the blockchain.
+//! 
+//! You need to implement the `Blockchain` struct and its methods.
+
+use crate::block::Block;
+use crate::crypto::hash::H256;
+use std::collections::HashMap;
+
+pub struct Blockchain {
+    // TODO: add fields here as you need, e.g.:
+    //
+    // hash_to_block: HashMap<H256, Block>,
+}
+
+impl Blockchain {
+    /// Create a new blockchain, only containing the genesis block
+    pub fn new() -> Self {
+        // TODO
+        Blockchain {
+            // TODO
+        }
+    }
+
+    /// Insert a block into blockchain
+    pub fn insert(&mut self, block: &Block) {
+        unimplemented!()
+    }
+
+    /// Get the last block's hash of the longest chain
+    pub fn tip(&self) -> H256 {
+        unimplemented!()
+    }
+
+    /// Get the last block's hash of the longest chain
+    #[cfg(any(test, test_utilities))]
+    pub fn all_blocks_in_longest_chain(&self) -> Vec<H256> {
+        unimplemented!()
+    }
+}
+
+
+#[cfg(any(test, test_utilities))]
+mod tests {
+    use super::*;
+    use crate::block::test::generate_random_block;
+    use crate::crypto::hash::Hashable;
+
+    #[test]
+    fn insert_one() {
+        let mut blockchain = Blockchain::new();
+        let genesis_hash = blockchain.tip();
+        let block = generate_random_block(&genesis_hash);
+        blockchain.insert(&block);
+        assert_eq!(blockchain.tip(), block.hash());
+    }
+
+    #[test]
+    fn mp1_insert_chain() {
+        let mut blockchain = Blockchain::new();
+        let genesis_hash = blockchain.tip();
+        let mut block = generate_random_block(&genesis_hash);
+        blockchain.insert(&block);
+        assert_eq!(blockchain.tip(), block.hash());
+        for _ in 0..50 {
+            let h = block.hash();
+            block = generate_random_block(&h);
+            blockchain.insert(&block);
+            assert_eq!(blockchain.tip(), block.hash());
+        }
+    }
+
+    #[test]
+    fn mp1_insert_3_fork_and_back() {
+        let mut blockchain = Blockchain::new();
+        let genesis_hash = blockchain.tip();
+        let block_1 = generate_random_block(&genesis_hash);
+        blockchain.insert(&block_1);
+        assert_eq!(blockchain.tip(), block_1.hash());
+        let block_2 = generate_random_block(&block_1.hash());
+        blockchain.insert(&block_2);
+        assert_eq!(blockchain.tip(), block_2.hash());
+        let block_3 = generate_random_block(&block_2.hash());
+        blockchain.insert(&block_3);
+        assert_eq!(blockchain.tip(), block_3.hash());
+        let fork_block_1 = generate_random_block(&block_2.hash());
+        blockchain.insert(&fork_block_1);
+        assert_eq!(blockchain.tip(), block_3.hash());
+        let fork_block_2 = generate_random_block(&fork_block_1.hash());
+        blockchain.insert(&fork_block_2);
+        assert_eq!(blockchain.tip(), fork_block_2.hash());
+        let block_4 = generate_random_block(&block_3.hash());
+        blockchain.insert(&block_4);
+        assert_eq!(blockchain.tip(), fork_block_2.hash());
+        let block_5 = generate_random_block(&block_4.hash());
+        blockchain.insert(&block_5);
+        assert_eq!(blockchain.tip(), block_5.hash());
+    }
+
+}
